d21d48f5b8f4f6b542a1f4bca5670354f3ae8419
Abu Shaleh <abushalehtoha@gmail.com>
2024-08-20 00:55:34 +0600

    Sign in Page

---
+++ sign-in.js
@@ -0,0 +1,19 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the form from submitting and reloading the page
+
+    const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+    
+    const name=firstName+lastName;
+    const pass=localStorage.getItem(email);
+    if(pass===password)
+        {
+            alert('Congratulations ! You have Successfully Log in..');
+        }
+    else{
+        alert("You Don't Have any Acconut , Please Sigb-Up First!!");
+    }
+
+});
