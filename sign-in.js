ba85eccd3ca82c39179eca299620d8d9cda1b578
Abu Shaleh <abushalehtoha@gmail.com>
2024-08-20 13:25:00 +0600

    used setinterval with the front text of h1 tag

--- sign-in.js
+++ sign-in.js
@@ -26,3 +26,30 @@ document.querySelector('form').addEventListener('submit', function(event) {
 
 
 });
+
+
+
+function getcolor()
+{
+    let aa=()=> Math.floor(Math.random()*256);
+    var color=`rgb(${aa()} , ${aa()} , ${aa()}`;
+    return color;
+
+}
+
+document.querySelector("#st1").style.backgroundColor="white"; // set background color to white
+let a = "Sign In With Your Email and Password";
+let b = "";
+let i = 0;  // Counter for tracking the position in the string
+
+let c = setInterval(() => {
+    if (i < a.length) {
+        let val = a[i];  // Get the current character
+        let col=getcolor();
+        b += `<span style="color:${col}">${val}</span>`;  // Append the character to the string `b`
+        document.querySelector("#st1").innerHTML = b;  // Update the HTML element
+        i++;  // Move to the next character
+    } else {
+        clearInterval(c);  // Stop the interval when the string is fully displayed
+    }
+}, 70);
